860f6350b7814a07e8d6e3cb77a11cb990327049
Robyn Choi <robyn3choi@gmail.com>
2020-11-18 20:54:49 -0800

    created project, implemented player movement, added joystick controls to input map

---
+++ Player/Player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+
+var velocity = Vector2.ZERO
+#onready var animation_player = $AnimationPlayer
+#onready var animation_tree = $AnimationTree
+#onready var animation_state = animation_tree.get("parameters/playback")
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
+	input_vector.y = Input.get_action_strength('ui_down') - Input.get_action_strength('ui_up')
+	input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+#		animation_tree.set("parameters/Idle/blend_position", input_vector)
+#		animation_tree.set("parameters/Run/blend_position", input_vector)
+#		animation_state.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+#		animation_state.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
